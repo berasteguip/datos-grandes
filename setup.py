4e9e0ddbc73f47b15b25f2f04bc3714db0435467
gonzalo334 <gonzalogarciame2005@gmail.com>
2026-01-22 18:55:51 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+import boto3
+from pathlib import Path
+
+s3 = boto3.client("s3", region_name="eu-south-2")
+
+bucket_name = "datos-grandes-eth-project"  # nombre global único
+
+s3.create_bucket(
+    Bucket=bucket_name,
+    CreateBucketConfiguration={
+        "LocationConstraint": "eu-south-2"
+    }
+)
+
+print("Bucket creado")
+
+s3 = boto3.client("s3")
+bucket = "datos-grandes-eth-project"
+
+base_path = Path("data")
+
+for csv_file in base_path.glob("eth_data_*.csv"):
+    year = csv_file.stem.split("_")[-1]   # 2022, 2023, ...
+    
+    s3_key = f"eth/freq=1d/year={year}/{csv_file.name}"
+    
+    s3.upload_file(
+        Filename=str(csv_file),
+        Bucket=bucket,
+        Key=s3_key
+    )
+    
+    print(f"Subido {s3_key}")
